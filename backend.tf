5b8927f3cf97cca5fc246df45f6ab868a75272f7
singhgu2 <gurpreet.singh1@ottawa.ca>
2020-08-13 17:04:20 -0400

    update

--- backend.tf
+++ backend.tf
@@ -3,5 +3,6 @@ terraform {
     resource_group_name   = "terraform-backend-self-hosted-rg"
     storage_account_name  = "storageacctsaagent"
     container_name        = "storagecontaineragent"
+    key                   = "terraform.tfstate"
   }
 }
